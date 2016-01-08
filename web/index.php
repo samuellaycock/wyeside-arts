d4571ff8c946795f34412d2e9c065ab8a1ad39e0
James <james.dobb@gmail.com>
2016-01-08 17:54:29 +0000

    moving to slim 2

--- web/index.php
+++ web/index.php
@@ -1,18 +1,43 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+/**
+ * @author James Dobb <james.dobb@gmail.com>
+ */
 
 define('APP_DIR', realpath(__DIR__ . '/../'));
 
+/**
+ * Load the app's environment
+ */
+$envFile = APP_DIR . '/config/environment.local.php';
+if (!file_exists($envFile)) {
+    throw new \Exception('Unable to run app! Please create the required config/environment.local.php file.');
+} else {
+    define('APP_ENV', require $envFile);
+}
+
+if (APP_ENV != 'production') {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
 require '../vendor/autoload.php';
 
-$di = new Slim\Container([
-    'settings' => require APP_DIR . '/config/config.php'
-]);
-$app = new Slim\App($di);
 
+$app = new Slim\Slim([
+    'mode' => APP_ENV,
+    'templates.path' => APP_DIR . '/views',
+    'view' => new \Slim\Views\Twig
+]);
+
+$app->get('/', function() use ($app) {
+    $app->render('frontend/index.twig');
+});
+
+$app->run();
+
+
+/**
 $routeLoader = new \App\Router\Loader($app);
 $routeLoader->loadRoutes(require(APP_DIR . '/config/routes.php'));
 
@@ -29,3 +54,4 @@ print_r($repo->getGenres());
 die();
 
 $app->run();
+ * */
