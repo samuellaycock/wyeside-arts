19abfa73682be1de5967474e10a4eccff5353fb2
James <james.dobb@gmail.com>
2016-01-11 17:09:14 +0000

    basic template to start work with

---
+++ src/App/Model/Repo/ShowingRepo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author James Dobb <james.dobb@gmail.com>
+ */
+
+namespace App\Model\Repo;
+
+
+use Doctrine\ORM\EntityRepository;
+
+
+class GenreRepo extends EntityRepository
+{
+
+
+
+}
