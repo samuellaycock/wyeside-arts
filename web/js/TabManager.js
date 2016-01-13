8cee630152d9ceb0a83d717828d868344c78fd38
James <james.dobb@gmail.com>
2016-01-13 14:54:43 +0000

    working tab manager

--- web/js/TabManager.js
+++ web/js/TabManager.js
@@ -12,12 +12,24 @@ TabManager.prototype.ready = function()
 {
     var me = this;
     $('.tab-object').click(function(){
-        me.loadPage(this);
+        me.changeTab(this);
     });
 };
 
 
-TabManager.prototype.changeTab = function(tabObject)
+TabManager.prototype.changeTab = function(selectedTab)
 {
+    var group = $(selectedTab).data('tabgroup');
+    var controls = $('.tab-object[data-tabgroup="'+group+'"]');
 
+    $.each(controls, function(i, tab){
+        var page = ($(tab).data('element'));
+        if(selectedTab == tab) {
+            $(tab).addClass('tab-active');
+            $("#" + page).addClass('tab-page-active');
+        }else{
+            $(tab).removeClass('tab-active');
+            $("#" + page).removeClass('tab-page-active');
+        }
+    });
 }
