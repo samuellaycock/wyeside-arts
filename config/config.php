9c7270f7916540bff3459f6c68521534616fb770
Samuel Laycock <samuel.paul.laycock@gmail.com>
2016-05-23 18:08:19 +0000

    Change navbar to have scroll effect and type

--- config/config.php
+++ config/config.php
@@ -8,7 +8,7 @@ return [
 
     'db.user' => 'wyeside',
     'db.host' => '127.0.0.1',
-    'db.password' => 'lemons12#%@',
-    'db.name' => 'wyeside'
+    'db.password' => 'lemons',
+    'db.name' => 'wyeside-db'
 
 ];
