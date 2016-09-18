3d75e57013f95c3978fd809a5aae40ca777a2204
James <james.dobb@gmail.com>
2016-09-18 23:10:50 +0100

    update

--- _build/db-001.sql
+++ _build/db-001.sql
@@ -1,5 +1,3 @@
-ALTER TABLE `eventdate`
-ADD COLUMN `ticketsolveId` VARCHAR(64) NULL AFTER `eventID`;
 
 ALTER TABLE `event`
 ADD COLUMN `eventTagline` VARCHAR(512) NULL DEFAULT NULL AFTER `eventTitle`;
@@ -13,3 +11,11 @@ CREATE TABLE `wyeside`.`config` (
 
 TRUNCATE `eventgenre`;
 INSERT INTO `eventgenre` VALUES (1,'Action'),(2,'Adventure'),(3,'Animation'),(4,'Biography'),(5,'Comedy'),(6,'Crime'),(7,'Documentary'),(8,'Drama'),(9,'Family'),(10,'Fantasy'),(11,'Film-Noir'),(12,'Game-Show'),(13,'History'),(14,'Horror'),(15,'Music'),(16,'Musical'),(17,'Mystery'),(18,'News'),(19,'Reality-TV'),(20,'Romance'),(21,'Sci-Fi'),(22,'Sport'),(23,'Talk-Show'),(24,'Thriller'),(25,'War'),(26,'Western');
+
+CREATE TABLE `config` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `value` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
