fadd703818e680ceeee01fa53e70bc03d91a5667
James Dobb <james.dobb@gmail.com>
2016-10-20 20:02:53 +0100

    updated config

--- config/production.php
+++ config/production.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'db.user' => 'root',
-    'db.host' => '127.0.0.1',
-    'db.password' => 'lemon',
-    'db.name' => 'wyeside',
+    'db.user' => 'wyeside-new',
+    'db.host' => 'freetiermysql.cp7qknwxxaia.eu-west-1.rds.amazonaws.com',
+    'db.password' => 'D3HXF-maC-q*GumvAU+g--yUFc&SDyqH%8+kjzbPp^2X$4nf^!eEJHp+WaTx6^2m',
+    'db.name' => 'wyeside-demo',
 
     'debug' => false
 
