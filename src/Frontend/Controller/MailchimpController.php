0bb3b074f1b99eeaa868e8b9bd319f4bfef016e1
James Dobb <james.dobb@gmail.com>
2017-01-30 23:14:42 +0000

    update

--- src/Frontend/Controller/MailchimpController.php
+++ src/Frontend/Controller/MailchimpController.php
@@ -36,12 +36,11 @@ class MailchimpController extends FrontendController
 
         try {
             $mailChimp = new MailChimp(self::API_KEY);
-            $responseData  = $mailChimp->post('lists/' . self::LIST_ID, json_encode([
-                [
+            $responseData  = $mailChimp->post('lists/' . self::LIST_ID, [
                     'email_address' => $this->app->request->post('email'),
                     'status' => 'subscribed'
                 ]
-            ]));
+            );
             if ($mailChimp->success()) {
                 $view = 1;
             } else {
