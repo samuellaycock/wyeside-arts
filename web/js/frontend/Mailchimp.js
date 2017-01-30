b0c136c990a1befc6ac074548c45a76ca71347ad
James Dobb <james.dobb@gmail.com>
2017-01-30 22:17:33 +0000

    update

--- web/js/frontend/Mailchimp.js
+++ web/js/frontend/Mailchimp.js
@@ -28,7 +28,7 @@ function Mailchimp() {
                 email: $(me.email).val()
             },
             complete: function (result) {
-                if (result == 1) {
+                if (result.responseText == 1) {
                     $(me.form).hide(0);
                     $(me.response).show(0);
                 } else {
