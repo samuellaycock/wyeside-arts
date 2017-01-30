f80846e9cd170b4a048a8a5a790dae4d8fe7eac0
James Dobb <james.dobb@gmail.com>
2017-01-30 21:56:53 +0000

    update

--- web/js/frontend/Mailchimp.js
+++ web/js/frontend/Mailchimp.js
@@ -10,33 +10,33 @@ function Mailchimp() {
     this.response = $("#email-response");
 
 
-    this.submit.on("click", function () {
+    $(this.submit).on("click", function () {
 
-        me.firstName.disable();
-        me.lastName.disable();
-        me.email.disable();
-        me.submit.hide(0);
-        me.message.show(0);
-        me.message.innerHTML('<img src="/img/frontend/spinner.gif">');
+        $(me.firstName).disable();
+        $(me.lastName.disable();
+        $(me.email).disable();
+        $(me.submit).hide(0);
+        $(me.message).show(0);
+        $(me.message).innerHTML('<img src="/img/frontend/spinner.gif">');
 
         $.ajax({
             url: '/email-subscribe',
             method: 'POST',
             data: {
-                firstName: me.firstName.val(),
-                lastName: me.lastName.val(),
-                email: me.email.val()
+                firstName: $(me.firstName).val(),
+                lastName: $(me.lastName).val(),
+                email: $(me.email).val()
             },
             success: function (result) {
                 if (result.success == 1) {
-                    me.form.hide(0);
-                    me.response.show(0);
+                    $(me.form).hide(0);
+                    $(me.response).show(0);
                 } else {
-                    me.firstName.enable();
-                    me.lastName.enable();
-                    me.email.enable();
-                    me.submit.enable(0);
-                    me.message.innerHTML('<p>Sorry, there was a problem subscribing you to our list.</p>');
+                    $(me.firstName).enable();
+                    $(me.lastName).enable();
+                    $(me.email).enable();
+                    $(me.submit).enable(0);
+                    $(me.message).innerHTML('<p>Sorry, there was a problem subscribing you to our list.</p>');
                 }
             }
         });
