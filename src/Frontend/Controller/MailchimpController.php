8f3dff0cf7c9abdae36d98ee2d11bbee64918165
James Dobb <james.dobb@gmail.com>
2017-01-30 22:55:21 +0000

    update

--- src/Frontend/Controller/MailchimpController.php
+++ src/Frontend/Controller/MailchimpController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Frontend\Controller;
+
 use DrewM\MailChimp\MailChimp;
 
 
@@ -24,19 +25,23 @@ class MailchimpController extends FrontendController
         $data['email_address'] = $this->app->request->post('email');
         $data['status'] = 'subscribed';
 
-       /* if (!empty($this->app->request->post('firstName', ''))) {
-            $data['firstName'] = $this->app->request->post('firstName');
-        }
+        /* if (!empty($this->app->request->post('firstName', ''))) {
+             $data['firstName'] = $this->app->request->post('firstName');
+         }
 
-        if (!empty($this->app->request->post('lastName', ''))) {
-            $data['lastName'] = $this->app->request->post('lastName');
-        }
-       */
+         if (!empty($this->app->request->post('lastName', ''))) {
+             $data['lastName'] = $this->app->request->post('lastName');
+         }
+        */
 
         try {
-           $mailChimp = new MailChimp(self::API_KEY);
-           $mailChimp->post('lists/' .  self::LIST_ID, ["members" => [$data]]);
-           $view = 1;
+            $mailChimp = new MailChimp(self::API_KEY);
+            $response = $mailChimp->post('lists/' . self::LIST_ID, ["members" => [$data]]);
+            if ($mailChimp->success()) {
+                $view = 1;
+            } else {
+                $view = $mailChimp->getLastError();
+            }
         } catch (\Exception $e) {
             $view = 0;
         }
